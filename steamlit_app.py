13b0402490950a39fe9af1b01f6a85ce394d16ad
Vsrinath2 <129857876+Vsrinath2@users.noreply.github.com>
2023-04-06 15:40:17 +0530

    Update steamlit_app.py
    #

--- steamlit_app.py
+++ steamlit_app.py
@@ -23,7 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('fruityvice search')
-try
+try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
   if not fruit_choice:
     streamlit.error('Please selcet a fruit to get information.')
