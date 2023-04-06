342500265aa838c25a70a249afce1b82cca1e130
Vsrinath2 <129857876+Vsrinath2@users.noreply.github.com>
2023-04-06 15:24:05 +0530

    Update steamlit_app.py
    #

--- steamlit_app.py
+++ steamlit_app.py
@@ -45,5 +45,5 @@ streamlit.dataframe(my_data_rows)
 
 
 streamlit.header('what fruit whould like to add ?')
-fruit_add_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_add_choice = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('The user added  ', fruit_add_choice)
